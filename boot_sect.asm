840167c7f04aeedcb05e618889c90021052dcdd6
Fernando Barboza <falandocomofernando@gmail.com>
2023-11-08 15:56:37 -0300

    change variable name

--- boot_sect.asm
+++ boot_sect.asm
@@ -3,14 +3,12 @@
 ;
 [org 0x7c00]                ; Tell the assembler where this code will be loaded
 
-mov si, HELLO_MSG           ; Use BX as a parameter to our function, so
+mov si, GREETINGS_MSG       ; Use BX as a parameter to our function, so
 call print_string           ; We can specify the address of a string.
-
-jmp $
 
 %include "print_string.asm"
 
-HELLO_MSG db "Welcome to the PutrefatOS!", 0
+GREETINGS_MSG db "*** Welcome to the PutrefatOS! ***", 0
 
 ; Padding and magic number.
 times 510 - ( $ - $$ ) db 0
