690f323226f12a35fed4f000e3ebb6e6d59e9e89
Fernando Barboza <falandocomofernando@gmail.com>
2023-11-08 16:30:47 -0300

    print hex working

--- boot_sect.asm
+++ boot_sect.asm
@@ -6,9 +6,14 @@
 mov si, GREETINGS_MSG       ; Use BX as a parameter to our function, so
 call print_string           ; We can specify the address of a string.
 
+mov dx, 0x1fb6
+call print_hex
+
 %include "print_string.asm"
+%include "print_hex.asm"
 
 GREETINGS_MSG db "*** Welcome to the PutrefatOS! ***", 0
+HEX_OUT db "0x0000", 0
 
 ; Padding and magic number.
 times 510 - ( $ - $$ ) db 0
--- print_hex.asm
+++ print_hex.asm
@@ -1,42 +1,41 @@
 print_hex:
-  pusha
-  mov bx, HEX_OUT
-  mov cx, 4
-
-  call print_string
-  popa
-  ret
+  mov si, HEX_OUT         ; Load the address of the HEX_OUT buffer into SI
+  xor cx, cx              ; Initialize a counter to keep track of the position in the buffer
 
 convert_loop:
-  pusha
-  mov ax, dx
-  and ax, 0Fh
+  mov ax, dx              ; Extract the less significant char (4-bits) from DX
+  and ax, 0Fh             ; Mask out all bits except the 4 less significant bits
 
-  call nibble_to_hex
+  call char_to_hex      ; Convert the char to its corresponding hexadecimal character
 
-  mov [bx], al
-  inc bx
-  shr dx,4
-  loop convert_loop
+  mov di, si              ; Store the hexadecimal character in the buffer
+  add di, cx              ; Calculate the address in DI
 
-  mov byte [bx], 0
-  call print_string
-  popa
-  ret
+  mov [di], al
 
-nibble_to_hex:
-  pusha
-  cmp al, 10
-  jl is_digit
-  add al, 7
-  popa
+  inc cx                  ; Move to the next char in the buffer
+  shr dx, 4               ; Shift the remaining bits in DX to the right by 4 bits
+
+  cmp cx, 4               ; Check if all 4 chars have been processed
+  je done
+
+  jmp convert_loop        ; If not, continue the loop
+
+char_to_hex:
+  ; Input: AL - Nibble (4 bits) value
+  ; Output: AL - Hexadecimal character '0'-'9' or 'A'-'F'
+  cmp al, 10              ; Compare AL value with 10
+  jl is_digit             ; Jump if char is less than 10
+  add al, 7               ; If its not a digit (10-15), convert to 'A'-'F'
 
 is_digit:
-  pusha
-  add al, '0'
-  popa
+  add al, '0'             ; Convert the char value to its ASCII character representation
   ret
 
-%include "print_string.asm"
+done:
+  mov di, si              ; Null-terminate the string
+  add di, cx              ; Calculate the address in DI
+  mov byte [di], 0
+  call print_string       ; Call the function to print the string pointed to by SI
+  ret
 
-HEX_OUT: db "0x0000", 0
